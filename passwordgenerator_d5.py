6d9e1d360c92ed180c469681815b79ac81567320
Kris <46921047+ToomKris@users.noreply.github.com>
2026-08-27 20:06:29 +0300

    Update passwordgenerator_d5.py

--- passwordgenerator_d5.py
+++ passwordgenerator_d5.py
@@ -4,9 +4,6 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')']
 
-passwordletter = []
-passwordnumber = []
-passwordsymbol = []
 password = []
 finalpass = ""
 print("Welcome to the PyPassword Generator!")
@@ -15,17 +12,14 @@ nr_symbol = int(input(f"How many symbols would you like?\n"))
 nr_number = int(input(f"How many numbers would you like?\n"))
 
 for let in range(nr_letter):
-    passwordletter.append(random.choice(letters))
+    password.append(random.choice(letters))
 
 for num in range(nr_number):
-    passwordnumber.append(random.choice(numbers))
+    password.append(random.choice(numbers))
 
 for sym in range(nr_symbol):
-    passwordsymbol.append(random.choice(symbols))
+    password.append(random.choice(symbols))
 
-password.extend(passwordletter)
-password.extend(passwordnumber)
-password.extend(passwordsymbol)
 random.shuffle(password)
 
 for things in password:
